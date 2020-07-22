2f182274b30596edb4eac45dda40af80c5a8b6c4
Giacomo Pasin <giacomo.pasin@gmail.com>
2020-07-22 11:24:11 +0200

    Added colors.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@
 
 import pygame
 
+from utils import colors
+
+
 # Basic Variables Setup
 
 WIDTH, HEIGHT = 1920, 1080
@@ -12,9 +15,20 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Breakoff')
 pygame.init()
 
+rect = pygame.Rect(WIDTH // 2 -50, HEIGHT // 2 -50, 100, 100)
+
 while isRunning:
 
     # Handle Input
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             isRunning = False
+
+    # Screen Background
+    screen.fill(colors.WHITE)
+
+    # Draw Items
+    pygame.draw.rect(screen, colors.RED, rect)
+
+    # Display everything on the screen
+    pygame.display.flip()
