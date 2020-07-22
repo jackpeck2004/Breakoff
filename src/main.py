bc9c811fc6030cbf92033ea3c306d677de84a417
Giacomo Pasin <giacomo.pasin@gmail.com>
2020-07-22 11:01:53 +0200

    Basic Pygame window working

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+import pygame
+
+# Basic Variables Setup
+
+WIDTH, HEIGHT = 1920, 1080
+isRunning = True
+
+# Initialize Pygame
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Breakoff')
+pygame.init()
+
+while isRunning:
+
+    # Handle Input
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            isRunning = False
