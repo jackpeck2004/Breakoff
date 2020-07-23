a750eff61b922ba7d9703a2e0cc56567e14c8553
Giacomo Pasin <jackpeck@Giacomos-MBP.homenet.telecomitalia.it>
2020-07-23 11:46:03 +0200

    created ball class

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 
 import pygame
-
 from utils import colors, classes
-
 from utils.rounded_rect import draw_rounded_rect
 
 
 # Basic Variables Setup
 
 WIDTH, HEIGHT = 1080, 720
+clock = pygame.time.Clock()
 isRunning = True
 
 # Initialize Pygame
@@ -24,6 +23,11 @@ PLAYER_COLOR = colors.BLUE
 player = classes.RectSprite(
     PLAYER_WIDTH, PLAYER_HEIGHT, WIDTH//2-PLAYER_WIDTH//2, HEIGHT-PLAYER_HEIGHT-PLAYER_HEIGHT//3)
 
+# Ball
+
+ball = classes.CircleSprite(100, WIDTH//2, HEIGHT//2)
+
+
 while isRunning:
 
     # Handle Input
@@ -35,10 +39,10 @@ while isRunning:
     screen.fill(colors.BLACK)
 
     # Draw Items
-    # pygame.draw.rect(screen, colors.RED, rect)
-    # player.draw(screen, colors.BLUE)
-    draw_rounded_rect(screen, player.rect, PLAYER_COLOR,
-                      player.rect.height//2-2)
+    # draw_rounded_rect(screen, player.rect, PLAYER_COLOR,
+    #                   player.rect.height//2-2)
+    ball.draw(screen, colors.RED)
 
     # Display everything on the screen
+    clock.tick(60)
     pygame.display.flip()
--- src/utils/classes.py
+++ src/utils/classes.py
@@ -1,9 +1,11 @@
 import pygame
 
+
 class MasterSprite(pygame.sprite.Sprite):
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
 
 class RectSprite(MasterSprite):
     def __init__(self, width, height, x, y):
@@ -14,7 +16,24 @@ class RectSprite(MasterSprite):
 
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
-    def draw(self, screen, color):
+    def draw(self, surface, color):
         self.color = color
-        self.screen = screen
-        pygame.draw.rect(self.screen, self.color, self.rect)
+        self.surface = surface
+        pygame.draw.rect(self.surface, self.color, self.rect)
+
+
+class CircleSprite(MasterSprite):
+    def __init__(self, diameter, x, y):
+        self.diameter = diameter
+        self.width = self.diameter
+        self.height = self.diameter
+        self.x = x
+        self.y = y
+
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def draw(self, surface, color):
+        self.color = color
+        self.surface = surface
+        pygame.draw.circle(self.surface, self.color,
+                           (self.x, self.y), self.diameter//2)
